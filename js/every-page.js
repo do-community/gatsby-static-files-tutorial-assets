f283a01287d37cb1926c5b4195114b461146ce40
Joshua Tzucker <joshua.tz@gmail.com>
2021-10-07 04:02:17 -0700

    Add files from tutorial draft

---
+++ js/every-page.js
@@ -0,0 +1,13 @@
+/**
+ * This file should load and execute inside of every page load
+ */
+console.log(
+`
+===================================
+||                               ||
+||             Hello!            ||
+||  I hope you like my website!  ||
+||                               ||
+===================================
+`
+);
